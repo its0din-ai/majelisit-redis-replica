4d7e8bb05edcbe605d9e85ddb7cbcb39d5a2a273
W. SYAFIUDDIN <encrypt0r-x@protonmail.com>
2023-06-30 00:24:22 +0700

    simplying

--- workspace/src/main.rs
+++ workspace/src/main.rs
@@ -1,57 +1,20 @@
 use std::io::{Read, Write};
-use std::net::{TcpListener, TcpStream};
-use std::thread;
-
-use log::{LevelFilter, error, info};
-use env_logger;
-
-fn hndlrz(mut strim: TcpStream) {
-    let mut buffz = [0; 1024];
-
-    loop {
-        match strim.read(&mut buffz) {
-            Ok(n) => {
-                if n == 0 {
-                    break;
-                }
-
-                let reqz = String::from_utf8_lossy(&buffz[..n]).to_uppercase();
-                let resz = if reqz.trim() == "PING" {
-                    "PONG\r\n".to_owned()
-                } else {
-                    format!("DEBUG => {}\r", reqz)
-                };
-
-                strim.write_all(resz.as_bytes()).unwrap();
-            }
-            Err(e) => {
-                error!("External Error: {}", e);
-                break;
-            }
-        }
-
-        buffz = [0; 1024];
-    }
-}
+use std::net::{TcpListener};
 
 fn main() {
-    env_logger::builder()
-        .filter_level(LevelFilter::Info)
-        .init();
 
     let liztnr = TcpListener::bind("0.0.0.0:6379").expect("Bind failed bang, port udh kepake?");
-
-    info!("Listening. . .");
+    println!("Lizten di port 6379 bang!");
 
     for strim in liztnr.incoming() {
         match strim {
-            Ok(strim) => {
-                thread::spawn(move || {
-                    hndlrz(strim);
-                });
+            Ok(mut strim) => {
+                let mut buf = [0; 512];
+                strim.read(&mut buf).unwrap();
+                strim.write("+PONG\r\n".as_bytes()).unwrap();
             }
             Err(e) => {
-                error!("Internal Error: {}", e);
+                println!("error: {}", e);
             }
         }
     }
